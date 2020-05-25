55e9157dd4fc719c57286b248eb15e35b0d6fc8b
Cristian Gerardo Hernandez Barrios <34076011+Crigerprogrammer@users.noreply.github.com>
2020-05-24 21:56:46 -0600

    Inicio
    Archivo de index

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Proyecto Base de Datos 2</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<header>
+	<?php
+		require 'views/navbar.php';
+	?>
+</header>
+<body>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/export.js"></script>
+ </body>
+</html>
